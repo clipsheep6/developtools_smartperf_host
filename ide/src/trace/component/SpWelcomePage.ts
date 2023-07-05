c01da52059f76fec8ec8c34926022e8aade0529c
zhangzepeng <zhangzepeng7@huawei.com>
2023-07-05 08:40:45 +0800

    ‘首页推荐，删除代码复原’
    Signed-off-by: zhangzepeng <zhangzepeng7@huawei.com>

--- ide/src/trace/component/SpWelcomePage.ts
+++ ide/src/trace/component/SpWelcomePage.ts
@@ -30,12 +30,25 @@ export class SpWelcomePage extends BaseElement {
             align-content: center;
             background: var(--dark-background5,#F6F6F6);
         }
+        .home-page{
+            margin-top: -100px;
+        }
         .lit-icon{
+            margin-top: 50px;
+            margin-left: 50px;
             content: var(--dark-img,url('img/pic.png'));
         }
         </style>
         <div>
+            <div class="home-page">
+            <p/>黄区域名: <a href="https://smartperf.rnd.huawei.com/smartperf/">https://smartperf.rnd.huawei.com/smartperf/</a></p>
+            <p/>绿区域名: <a href="https://devecotesting.rnd.huawei.com/smartperf/">https://devecotesting.rnd.huawei.com/smartperf/</a></p>
+            <p/>3ms社区: <a href="http://3ms.huawei.com/km/groups/3956611/home?|=zh-cn">http://3ms.huawei.com/km/groups/3956611/home?|=zh-cn</a></p>
+            <p/>welink讨论群: 群1: 473395703, 群2: 485625665</p>
+            </div>
             <img class="lit-icon" >
+
+            
         </div>
         `;
   }
