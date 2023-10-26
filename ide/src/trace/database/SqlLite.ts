79eccd350f1218fdaf3ddb31d8f3750e1bc685b7
lizhuosong@huawei.com <lizhuosong@huawei.com>
2023-10-26 15:11:21 +0800

    格式化代码
    Signed-off-by: lizhuosong@huawei.com <lizhuosong@huawei.com>

--- ide/src/trace/database/SqlLite.ts
+++ ide/src/trace/database/SqlLite.ts
@@ -5496,19 +5496,19 @@ export const getTabRunningPercent = (tIds: Array<number>, leftNS: number, rightN
   query<SelectionData>(
     'getTabRunningPercent',
     `
-  select
-    B.pid,B.tid,B.state,B.cpu,B.dur,B.ts
-  from
-    thread_state AS B
-  left join 
-    trace_range AS TR
-  where
-  B.tid in (${tIds.join(',')})
-  and
-  B.state='Running'
-  and
-    not ((B.ts - TR.start_ts + ifnull(B.dur,0) < ${leftNS}) or (B.ts - TR.start_ts > ${rightNS}))
-    order by ts
+      select
+        B.pid,B.tid,B.state,B.cpu,B.dur,B.ts
+      from
+        thread_state AS B
+      left join 
+        trace_range AS TR
+      where
+        B.tid in (${tIds.join(',')})
+      and
+        B.state='Running'
+      and
+        not ((B.ts - TR.start_ts + ifnull(B.dur,0) < ${leftNS}) or (B.ts - TR.start_ts > ${rightNS}))
+      order by ts
   `,
     { $leftNS: leftNS, $rightNS: rightNS }
   );
@@ -5517,11 +5517,35 @@ export const querySearchFuncData = (funcName: string, tIds: Array<number>, leftN
   query(
     'querySearchFuncData',
     `
-  select c.cookie,c.id,c.name as funName,c.ts - r.start_ts as startTime,c.dur,c.depth,t.tid,t.name as threadName,
-  p.pid,'func' as type from callstack c left join thread t on c.callid = t.id left join process p on t.ipid = p.id
-  left join trace_range r
-  where c.name = '${funcName}' and t.tid = ${tIds} and
-  not ((startTime < ${leftNS}) or (startTime > ${rightNS}));
+      select 
+        c.cookie,
+        c.id,
+        c.name as funName,
+        c.ts - r.start_ts as startTime,
+        c.dur,
+        c.depth,
+        t.tid,
+        t.name as threadName,
+        p.pid,
+        'func' as type 
+      from 
+        callstack c 
+      left join 
+        thread t 
+      on 
+        c.callid = t.id 
+      left join 
+        process p 
+      on 
+        t.ipid = p.id
+      left join 
+        trace_range r
+      where 
+        c.name = '${funcName}' 
+      and 
+        t.tid = ${tIds} 
+      and
+        not ((startTime < ${leftNS}) or (startTime > ${rightNS}));
   `,
     { $search: funcName }
   );
@@ -5530,15 +5554,15 @@ export const queryCpuFreqUsageData = (Ids: Array<number>): Promise<Array<any>> =
   query(
     'queryCpuFreqUsageData',
     `select
-  value,
-  ifnull(dur,tb.end_ts - c.ts) dur,
-  ts-tb.start_ts as startNS,
-  filter_id
-  from
-  measure c,
-  trace_range tb
-  where
-  c.filter_id in (${Ids.join(',')})
+      value,
+      ifnull(dur,tb.end_ts - c.ts) dur,
+      ts-tb.start_ts as startNS,
+      filter_id
+    from
+      measure c,
+      trace_range tb
+    where
+      c.filter_id in (${Ids.join(',')})
   `
   );
 
@@ -5546,13 +5570,13 @@ export const queryCpuFreqFilterId = (): Promise<Array<any>> =>
   query(
     'queryCpuFreqFilterId',
     `select
-  id,
-  cpu
-  from
-  cpu_measure_filter 
-  where
-  name='cpufreq'
-  or
-  name='cpu_frequency'
+      id,
+      cpu
+    from
+      cpu_measure_filter 
+      where
+      name='cpufreq'
+    or
+      name='cpu_frequency'
   `
   );
