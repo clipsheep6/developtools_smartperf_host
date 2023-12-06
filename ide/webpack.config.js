1049fd244f0b91b749827e5f52d00e0ed5e7a36c
zhangzepeng <zhangzepeng7@huawei.com>
2023-12-06 14:05:07 +0800

    ‘codecheck’
    Signed-off-by: zhangzepeng <zhangzepeng7@huawei.com>

--- ide/webpack.config.js
+++ ide/webpack.config.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require('path');
@@ -42,11 +57,11 @@ const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader
   console.log('start compile server');
   let outPath = path.normalize(path.join(__dirname, '/', 'dist'));
   let serverSrc = path.normalize(path.join(__dirname, '/server/main.go'));
-  if (!directoryExists(outPath)){
+  if (!directoryExists(outPath)) {
     runCommand(`mkdir ${outPath}`);
   } else {
-    runCommand(`rm  -rf ${outPath}/* `).then((result)=>{
-      
+    runCommand(`rm  -rf ${outPath}/* `).then((result) => {
+
     });
   }
   let rs;
