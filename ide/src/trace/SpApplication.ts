56d99103121a3be5b636c752f6854c32b60d8bb4
danghongquan <danghongquan3@huawei.com>
2023-12-11 15:57:19 +0800

    Keyboard_repair
    Signed-off-by: danghongquan <danghongquan3@huawei.com>

--- ide/src/trace/SpApplication.ts
+++ ide/src/trace/SpApplication.ts
@@ -1448,12 +1448,12 @@ export class SpApplication extends BaseElement {
                     },
                   },
                   {
-                    title: 'Keyboard shortcuts',
+                    title: 'Keyboard Shortcuts',
                     icon: 'smart-help',
                     clickHandler: function (item: MenuItem) {
                       SpStatisticsHttpUtil.addOrdinaryVisitAction({
-                        event: 'Keyboard shortcuts',
-                        action: 'Keyboard shortcuts',
+                        event: 'Keyboard Shortcuts',
+                        action: 'Keyboard Shortcuts',
                       });
                       that.search = false;
                       showContent(SpKeyboard);
@@ -1876,14 +1876,14 @@ export class SpApplication extends BaseElement {
             },
           },
           {
-            title: 'Keyboard shortcuts',
+            title: 'Keyboard Shortcuts',
             icon: 'smart-help',
             clickHandler: function (item: MenuItem) {
               that.search = false;
               showContent(SpKeyboard);
               SpStatisticsHttpUtil.addOrdinaryVisitAction({
-                event: 'Keyboard shortcuts',
-                action: 'Keyboard shortcuts',
+                event: 'Keyboard Shortcuts',
+                action: 'Keyboard Shortcuts',
               });
             },
           },
