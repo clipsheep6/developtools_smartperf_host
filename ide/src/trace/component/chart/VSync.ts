28831437323bb92f905477ad8c000275f40c3a36
hanlibin <hanlibin3@huawei.com>
2024-03-18 11:13:09 +0800

    fix:根据VSync类型绘制VSync信号
    Signed-off-by: hanlibin <hanlibin3@huawei.com>

--- ide/src/trace/component/chart/VSync.ts
+++ ide/src/trace/component/chart/VSync.ts
@@ -24,11 +24,9 @@ interface VSyncData {
 let vSyncDataList: VSyncData[] = [];
 let vSyncEnable = false;
 let isSingle = false;
-let isQuery = false;
 
 export function resetVSync(): void {
   vSyncEnable = false;
-  isQuery = false;
 }
 
 export const querySfVSyncData = (): Promise<Array<VSyncData>> =>
@@ -90,7 +88,6 @@ export async function setVSyncData(): Promise<void> {
     }
   });
   vSyncDataList = sfvSyncData;
-  isQuery = true;
 }
 
 /**
@@ -134,12 +131,10 @@ export function drawVSync(ctx: CanvasRenderingContext2D, width: number, height: 
  * enable/disable SingleVSync
  */
 export function enableVSync(press: boolean, ev: KeyboardEvent, handler?: Function): void {
-  if (!isQuery) {
+  if (ev.key.toLocaleLowerCase() === 'v' && !ev.ctrlKey) {
     window.publish(window.SmartEvent.UI.Loading, { loading: true, text: 'Query VSync' });
     setVSyncData();
     window.publish(window.SmartEvent.UI.Loading, { loading: false, text: 'Query VSync' });
-  }
-  if (ev.key.toLocaleLowerCase() === 'v' && !ev.ctrlKey) {
     vSyncEnable = !vSyncEnable;
     handler?.();
   }
