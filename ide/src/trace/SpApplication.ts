4ffff7ca7036df432eec80fe618784be0fdeadc3
wangyujie <wangyujie38@huawei.com>
2024-06-01 14:46:53 +0800

    fix:修复longtrace图标消失bug
    Signed-off-by: wangyujie <wangyujie38@huawei.com>

--- ide/src/trace/SpApplication.ts
+++ ide/src/trace/SpApplication.ts
@@ -1547,7 +1547,10 @@ export class SpApplication extends BaseElement {
     this.litSearch!.clear();
     Utils.currentSelectTrace = undefined;
     this.markJson = undefined;
+    console.log(multiTrace)
+    if(!multiTrace){
     this.longTracePage!.style.display = 'none';
+    }
     this.litSearch?.removeAttribute('distributed');
     SpStatisticsHttpUtil.addOrdinaryVisitAction({
       event: 'open_trace',
