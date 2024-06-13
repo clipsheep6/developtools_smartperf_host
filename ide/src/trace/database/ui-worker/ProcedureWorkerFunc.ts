4a4a0aa99c37d3382a142a9609f89e3c53da999d
wangziyi <wangziyi40@huawei.com>
2024-06-13 17:19:11 +0800

    修改丢帧计算和连线显示问题以及点击remove标记数据不消失
    Signed-off-by: wangziyi <wangziyi40@huawei.com>

--- ide/src/trace/database/ui-worker/ProcedureWorkerFunc.ts
+++ ide/src/trace/database/ui-worker/ProcedureWorkerFunc.ts
@@ -234,7 +234,7 @@ export class FuncStruct extends BaseFuncStruct {
       } else {
         ctx.globalAlpha = 1;
         //h、g异步方法颜色
-        if (data.callid && data.threadName) {
+        if (data.threadName) {
           if (data.funName!.startsWith('XStream')) {
             ctx.fillStyle = '#7a8c22';
           } else if (data.funName!.startsWith('WU-')) {
@@ -252,7 +252,7 @@ export class FuncStruct extends BaseFuncStruct {
         ctx.fillRect(data.frame.x, data.frame.y, data.frame.width, data.frame.height);
         if (data.frame.width > 10) {
           //h、g异步方法字体颜色 
-          if (data.callid && data.threadName) {
+          if (data.threadName) {
             if (data.funName!.startsWith('XStream') || data.funName!.startsWith('WU-')) {
               ctx.fillStyle = '#fff';
             } else {
