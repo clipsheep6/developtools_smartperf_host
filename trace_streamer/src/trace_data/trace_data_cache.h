fee870cfaf46f907a6f75a3cf49df323d1ac3fd8
jichuan <jichuan1@huawei.com>
2024-07-09 14:22:34 +0800

    feat: 同步
    Signed-off-by: jichuan <jichuan1@huawei.com>

--- trace_streamer/src/trace_data/trace_data_cache.h
+++ trace_streamer/src/trace_data/trace_data_cache.h
@@ -90,7 +90,7 @@ private:
     bool appStartTraceEnabled_ = false;
     bool binderRunnableTraceEnabled_ = false;
     bool HMKernelTraceEnabled_ = false;
-    bool rawTraceCutStartTsEnabled_ = false;
+    bool rawTraceCutStartTsEnabled_ = true;
     uint64_t splitFileMinTs_ = INVALID_UINT64;
     uint64_t splitFileMaxTs_ = INVALID_UINT64;
     std::deque<std::unique_ptr<std::string>> hookCommProtos_;
