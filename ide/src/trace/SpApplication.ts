29c264be89765e094430ed9606015edb5a0f6c07
wupoli3@huawei.com <wupoli3@huawei.com>
2024-07-11 13:59:57 +0800

    search 优化代码提交
    Signed-off-by: wupoli3@huawei.com <wupoli3@huawei.com>

--- ide/src/trace/SpApplication.ts
+++ ide/src/trace/SpApplication.ts
@@ -2015,19 +2015,11 @@ export class SpApplication extends BaseElement {
     this.spSystemTrace?.addEventListener('trace-previous-data', (ev) => {
       if(this.progressEL!.loading) {
         return;
-      }
-      if(this.litSearch!.isSearchInputFocus) {
-        this.litSearch!.isSearchInputFocus = !this.litSearch!.isSearchInputFocus;
-        return;
-      }
+      } 
       this.litSearch!.index = this.spSystemTrace!.showStruct(true, this.litSearch!.index, this.litSearch!.list);
     });
     this.spSystemTrace?.addEventListener('trace-next-data', (ev) => {
       if(this.progressEL!.loading) {
-        return;
-      }
-      if(this.litSearch!.isSearchInputFocus) {
-        this.litSearch!.isSearchInputFocus = !this.litSearch!.isSearchInputFocus;
         return;
       }
       this.litSearch!.index = this.spSystemTrace!.showStruct(false, this.litSearch!.index, this.litSearch!.list);
