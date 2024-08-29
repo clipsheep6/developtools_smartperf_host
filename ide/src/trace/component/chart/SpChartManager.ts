86293a63b876673d3ced775c33e3cb9d9e74dc53
zhangyan <zhangyan470@huawei.com>
2024-08-29 10:02:19 +0800

    feat:flag对应的Hangs区域添加描述
    Signed-off-by: zhangyan <zhangyan470@huawei.com>

--- ide/src/trace/component/chart/SpChartManager.ts
+++ ide/src/trace/component/chart/SpChartManager.ts
@@ -189,7 +189,7 @@ export class SpChartManager {
     await this.spHiSysEvent.init();
     let idAndNameArr = await queryDmaFenceIdAndCat();
     this.handleDmaFenceName(idAndNameArr as { id: number; cat: string; seqno: number; driver: string; context: string }[]);
-    if (FlagsConfig.getFlagsConfigEnableStatus("Hangs")) {
+    if (FlagsConfig.getFlagsConfigEnableStatus("Hanging Detection")) {
       progress('Hang init', 80);
       await this.hangChart.init();
     }
