4a3cd5c76aa52b0febcef62983f46b0ec2528d2e
danghongquan <danghongquan3@huawei.com>
2024-11-27 15:50:14 +0800

    fix:设备连接界面版本号默认显示修改
    Signed-off-by: danghongquan <danghongquan3@huawei.com>

--- ide/src/trace/component/SpRecordTrace.ts
+++ ide/src/trace/component/SpRecordTrace.ts
@@ -81,7 +81,7 @@ export class SpRecordTrace extends BaseElement {
   public static selectVersion: string | null;
   public static isVscode = false;
   public static cancelRecord = false;
-  static supportVersions = ['3.2', '4.0+', '5.0+'];
+  static supportVersions = ['unknown','3.2', '4.0+', '5.0+'];
   public deviceSelect: HTMLSelectElement | undefined;
   public deviceVersion: HTMLSelectElement | undefined;
   private _menuItems: Array<MenuItem> | undefined;
@@ -589,6 +589,7 @@ export class SpRecordTrace extends BaseElement {
 
   disconnectButtonClickEvent = (): void => {
     // --------------我修改的
+    this.setDeviceVersionSelect('unknown')
     let index = this.deviceSelect!.selectedIndex;
     if (index !== -1 && this.deviceSelect!.options.length > 0) {
       for (let i = 0; i < this.deviceSelect!.options.length; i++) {
@@ -660,7 +661,7 @@ export class SpRecordTrace extends BaseElement {
   private nativeMemoryHideBySelectVersion(): void {
     let divConfigs = this.spAllocations?.shadowRoot?.querySelectorAll<HTMLDivElement>('.version-controller');
     if (divConfigs) {
-      if (SpRecordTrace.selectVersion !== '3.2') {
+      if (SpRecordTrace.selectVersion !== '3.2' && SpRecordTrace.selectVersion !=='unknown') {
         for (let divConfig of divConfigs) {
           divConfig!.style.zIndex = '1';
         }
@@ -681,11 +682,11 @@ export class SpRecordTrace extends BaseElement {
     SpRecordTrace.supportVersions.forEach((supportVersion) => {
       let option = document.createElement('option');
       option.className = 'select';
-      option.selected = supportVersion === '4.0+';
-      option.textContent = `OpenHarmony-${supportVersion}`;
+      option.selected = supportVersion === 'unknown';
+      option.textContent = supportVersion === 'unknown'? 'unknown' : `OpenHarmony-${supportVersion}`;
       option.setAttribute('device-version', supportVersion);
       this.deviceVersion!.append(option);
-      SpRecordTrace.selectVersion = '4.0+';
+      SpRecordTrace.selectVersion = 'unknown';
       this.nativeMemoryHideBySelectVersion();
     });
   }
