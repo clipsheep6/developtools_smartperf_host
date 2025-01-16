0c2c946a7e9784c2940baad1635fc0eca9e0e95c
wangyujie <wangyujie38@huawei.com>
2025-01-16 16:52:57 +0800

    fix:修复tab页随鼠标拖拽异常
    Signed-off-by: wangyujie <wangyujie38@huawei.com>

--- ide/src/trace/component/SpSystemTrace.ts
+++ ide/src/trace/component/SpSystemTrace.ts
@@ -972,6 +972,18 @@ export class SpSystemTrace extends BaseElement {
 
   documentOnMouseOut = (ev: MouseEvent): void => spSystemTraceDocumentOnMouseOut(this, ev);
 
+  documentOnPointercancel = (ev: any): void => {//@ts-ignore
+    if (ev.pointerType == 'mouse' && (window as unknown).isSheetMove) {
+      this.dispatchEvent(
+        new CustomEvent('abnormal-mouseup', {
+          detail: {
+            value: true,
+          },
+        })
+      );
+    }
+  };
+
   keyPressMap: Map<string, boolean> = new Map([
     ['w', false],
     ['s', false],
@@ -1760,6 +1772,7 @@ export class SpSystemTrace extends BaseElement {
     this.addEventListener('mousedown', this.documentOnMouseDown);
     this.addEventListener('mouseup', this.documentOnMouseUp);
     this.addEventListener('mouseout', this.documentOnMouseOut);
+    this.addEventListener('pointercancel', this.documentOnPointercancel);
     document.addEventListener('keydown', this.documentOnKeyDown);
     document.addEventListener('keypress', this.documentOnKeyPress);
     document.addEventListener('keyup', this.documentOnKeyUp);
@@ -2176,6 +2189,7 @@ export class SpSystemTrace extends BaseElement {
     this.removeEventListener('mousedown', this.documentOnMouseDown);
     this.removeEventListener('mouseup', this.documentOnMouseUp);
     this.removeEventListener('mouseout', this.documentOnMouseOut);
+    this.removeEventListener('pointercancel', this.documentOnPointercancel);
     document.removeEventListener('keypress', this.documentOnKeyPress);
     document.removeEventListener('keydown', this.documentOnKeyDown);
     document.removeEventListener('keyup', this.documentOnKeyUp);
