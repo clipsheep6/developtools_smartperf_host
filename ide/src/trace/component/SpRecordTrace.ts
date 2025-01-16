60c62ba3716246cf451897bdd6609537868a0d9b
danghongquan <danghongquan3@huawei.com>
2025-01-16 15:54:56 +0800

    fix:优化抓取trace界面错误提示
    Signed-off-by: danghongquan <danghongquan3@huawei.com>

--- ide/src/trace/component/SpRecordTrace.ts
+++ ide/src/trace/component/SpRecordTrace.ts
@@ -583,7 +583,9 @@ export class SpRecordTrace extends BaseElement {
         this.progressEL!.loading = false;// @ts-ignore
         let errorMsg = new TextDecoder().decode(result);
         this.useExtentTip!.style.display = 'block';
-        this.useExtentTip!.innerHTML = errorMsg;
+        let urlAsciiArr = [104,116,116,112,115,58,47,47,119,105,107,105,46,104,117,97,119,101,105,46,99,111,109,47,100,111,109,97,105,110,115,47,55,54,57,49,49,47,119,105,107,105,47,49,50,53,52,56,48,47,87,73,75,73,50,48,50,53,48,49,49,54,53,55,53,48,52,53,52];
+        let exceptGuid = String.fromCodePoint(...urlAsciiArr);//'https://wiki.huawei.com/domains/76911/wiki/125480/WIKI202501165750454'
+        this.useExtentTip!.innerHTML = `抓取trace异常：${errorMsg} 可根据[<span style='cursor:pointer;'><a href=${exceptGuid} syule = 'color:blue;' target='_blank'>常见异常处理</a></span>]解决异常`;
         this.refreshDisableStyle(false, false);
         this.recordButton!.hidden = false;
         this.sp!.search = false;
@@ -973,7 +975,7 @@ export class SpRecordTrace extends BaseElement {
       let option = document.createElement('option');
       option.className = 'select';
       option.selected = supportVersion === '5.0+';
-      option.textContent =`OpenHarmony-${supportVersion}`;
+      option.textContent = `OpenHarmony-${supportVersion}`;
       option.setAttribute('device-version', supportVersion);
       this.deviceVersion!.append(option);
       SpRecordTrace.selectVersion = '5.0+';
