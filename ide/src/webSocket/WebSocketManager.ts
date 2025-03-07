7f13a693f857d2b2ad820c69aa355a221af639e5
danghongquan <danghongquan3@huawei.com>
2025-03-07 13:59:02 +0800

    fix:扩展服务升级
    Signed-off-by: danghongquan <danghongquan3@huawei.com>

--- ide/src/webSocket/WebSocketManager.ts
+++ ide/src/webSocket/WebSocketManager.ts
@@ -142,7 +142,7 @@ export class WebSocketManager {
     updateMessage(decode: MessageParam): void {
         if (decode.cmd === Constants.GET_VERSION_CMD) {
             // 小于则升级
-            let targetVersion = '1.1.0';
+            let targetVersion = '1.1.1';
             let currentVersion = new TextDecoder().decode(decode.data);
             let result = this.compareVersion(currentVersion, targetVersion);
             if (result === -1) {
