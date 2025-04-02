e78d84fd619797d1c3b12036e255a031044ab3f3
wangziyi <wangziyi40@huawei.com>
2025-04-02 10:28:47 +0800

    fix:修改再次导入so页面不跳转问题
    Signed-off-by: wangziyi <wangziyi40@huawei.com>

--- ide/src/trace/component/trace/base/TraceSheet.ts
+++ ide/src/trace/component/trace/base/TraceSheet.ts
@@ -104,8 +104,8 @@ import { XpowerThreadInfoStruct } from '../../../database/ui-worker/ProcedureWor
 import { TabPaneXpowerThreadInfoSelection } from '../sheet/xpower/TabPaneXpowerThreadInfoSelection';
 import { TabPaneXpowerGpuFreqSelection } from '../sheet/xpower/TabPaneXpowerGpuFreqSelection';
 import { XpowerGpuFreqStruct } from '../../../database/ui-worker/ProcedureWorkerXpowerGpuFreq';
-import { WebSocketManager} from '../../../../webSocket/WebSocketManager';
-import { Constants, TypeConstants} from '../../../../webSocket/Constants';
+import { WebSocketManager } from '../../../../webSocket/WebSocketManager';
+import { Constants, TypeConstants } from '../../../../webSocket/Constants';
 import { PerfFunctionAsmParam } from '../../../bean/PerfAnalysis';
 import { info, error } from '../../../../log/Log';
 import { XpowerThreadCountStruct } from '../../../database/ui-worker/ProcedureWorkerXpowerThreadCount';
@@ -138,6 +138,7 @@ export class TraceSheet extends BaseElement {
   private spsystemTrace: SpSystemTrace | undefined;
   private isDragging = true;
   private REQ_BUF_SIZE = 1024 * 1024;
+  private loadSoComplete = false;
 
   static get observedAttributes(): string[] {
     return ['mode'];
@@ -613,12 +614,13 @@ export class TraceSheet extends BaseElement {
         }
         if (fileList.length > 0) {
           importFileBt!.disabled = true;
+          this.loadSoComplete = false;
           window.publish(window.SmartEvent.UI.Loading, { loading: true, text: 'Import So File' });
           this.uploadSoOrAN(fileList).then(r => {
             // @ts-ignore
             document.querySelector('body > sp-application').shadowRoot.querySelector('#sp-system-trace').shadowRoot.querySelector('div > trace-sheet').shadowRoot.querySelector('#box-perf-analysis > tabpane-perf-analysis').shadowRoot.querySelector('#SO-err-tips')?.innerHTML = '';
-            let  soFileList = fileList.filter(item => !item.name.includes('.an'));
-            if(soFileList.length === 0) {
+            let soFileList = fileList.filter(item => !item.name.includes('.an'));
+            if (soFileList.length === 0) {
               window.publish(window.SmartEvent.UI.UploadSOFile, {});
               importFileBt!.disabled = false;
               return;
@@ -631,6 +633,7 @@ export class TraceSheet extends BaseElement {
                 importFileBt!.disabled = false; // @ts-ignore
                 if (res.result === 'ok') {
                   window.publish(window.SmartEvent.UI.UploadSOFile, {});
+                  this.loadSoComplete = true;
                 } else {
                   // @ts-ignore
                   const failedList = res.failedArray.join(',');
@@ -1183,6 +1186,12 @@ export class TraceSheet extends BaseElement {
       });
     if (restore) {
       if (this.litTabs?.activekey) {
+        if (this.loadSoComplete) {
+          let analysisTabpane = this.shadowRoot!.querySelector('#box-perf-analysis') as LitTabpane;
+          if (analysisTabpane && this.litTabs) {
+            this.litTabs.activekey = analysisTabpane.key;
+          }
+        }
         this.loadTabPaneData(this.litTabs?.activekey);
         this.setMode('max');
         return true;
