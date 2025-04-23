0d1f999418b8d44f766b0df1e63482b25a75f6b6
JustinYT <liyiting12@huawei.com>
2025-04-23 17:55:16 +0800

    feat:内核trace数据结构优化，trace新增增级别、tag、cat、args适配 2.框选Occurrences汇总数据bug修复
    Signed-off-by: JustinYT <liyiting12@huawei.com>

--- ide/src/trace/component/trace/sheet/TabPaneCurrentSelection.ts
+++ ide/src/trace/component/trace/sheet/TabPaneCurrentSelection.ts
@@ -473,6 +473,7 @@ export class TabPaneCurrentSelection extends BaseElement {
         }
         list.push(item);
       });
+      this.addTabSliceDetail(data, list)
       this.currentSelectionTbl!.dataSource = list;
       // @ts-ignore
       let startTimeAbsolute = (data.startTs || 0) + (window as unknown).recordStartNS;
@@ -494,6 +495,7 @@ export class TabPaneCurrentSelection extends BaseElement {
         name: 'Thread',
         value: (this.transferString(threadName ?? '') || 'NULL') + ' [' + dataTid + '] ',
       });
+      this.addTabSliceDetail(data, list)
       list.push({
         name: 'StartTime(Relative)',
         value: getTimeString(data.startTs || 0),
@@ -599,6 +601,7 @@ export class TabPaneCurrentSelection extends BaseElement {
       argset.forEach((item) => {
         list.push({ name: item.keyName, value: item.strValue });
       });
+      this.addTabSliceDetail(data, list)
       this.addTabPanelContent(list, data, information);
       this.currentSelectionTbl!.dataSource = list;
     });
@@ -653,6 +656,7 @@ export class TabPaneCurrentSelection extends BaseElement {
         }
         );
       }
+      this.addTabSliceDetail(data, list)
       this.addTabPanelContent(list, data, information);
       this.currentSelectionTbl!.dataSource = list;
       let funcClick = this.currentSelectionTbl?.shadowRoot?.querySelector('#function-jump');
@@ -684,6 +688,21 @@ export class TabPaneCurrentSelection extends BaseElement {
         }
       });
     });
+  }
+
+  private addTabSliceDetail(data: FuncStruct, list: unknown[]) {
+    const properties = [
+      {key: 'trace_level', name: 'TraceLevel'},
+      {key: 'trace_tag', name: 'TraceTag'},
+      {key: 'category', name: 'Category'},
+      {key: 'custom_args', name: 'CustomArgs'},
+    ];
+    properties.forEach(prop => {
+      if (data[prop.key] && list !== undefined) {
+        list!.push({name: prop.name, value: data[prop.key]});
+      }
+    });
+    return list;
   }
 
   private handleAsyncBinder(
@@ -748,6 +767,7 @@ export class TabPaneCurrentSelection extends BaseElement {
           value: (this.transferString(threadName ?? '') || 'NULL') + ' [' + data.tid + '] ',
         });
       }
+      this.addTabSliceDetail(data, list)
       this.addTabPanelContent(list, data, information);
       this.currentSelectionTbl!.dataSource = list;
       let funcClick = this.currentSelectionTbl?.shadowRoot?.querySelector('#function-jump');
