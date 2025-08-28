b15be4acd16aa53dafb4ded54f4a8928aadde256
牛国亮 <niuguoliang4@huawei.com>
2025-08-28 20:48:00 +0800

    修改告警
    Signed-off-by: 牛国亮 <niuguoliang4@huawei.com>

--- smartperf_device/device_ui/entry/src/main/ets/common/entity/LocalConfigEntity.ts
+++ smartperf_device/device_ui/entry/src/main/ets/common/entity/LocalConfigEntity.ts
@@ -12,8 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { CONFIG } from './ConfigReadmeUrl';
-
 export enum TestMode {
   ONLINE,
   BRIGHTNESS,
@@ -147,10 +145,12 @@ export class QuestionItem {
   }
 }
 
+const configReadme = 'developtools_smartperf_host/smartperf_device/device_ui/README_zh.md';
+
 export const questionList = new Array(
   new QuestionItem(
     '1.SP工具怎么使用',
-    '有关详细指南, 请访问文档: ${configReadme.SP_README_HELP_URL}'
+    '有关详细指南, 访问路径OpenHarmony仓: ${configReadme}'
   ),
   new QuestionItem('2.SP工具支持FPS采集吗?', '可以, fps依赖Hidumper能力..'),
   new QuestionItem('3.SP工具支持RAM采集吗?', 'ram采集目前是 读取进程节点内存信息中的PSS值...'),
